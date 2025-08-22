d483506f8f30887787a14735c9472973e58a2ba6
ArtiyxDev <220812622+ArtiyxDev@users.noreply.github.com>
2025-08-22 17:06:13 -0500

    refactor: remove autoprefixer from Vite config and clean up dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwind from "@tailwindcss/vite";
-import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
 });
